692311f0783402185d418788474042746beaf7ad
Miriam Burfeind <miriamburfeind@83adc3af.funky.uni-osnabrueck.de>
2019-07-31 09:44:34 +0200

    package clean up

--- imports/client/ui/student/vektors/SchoolFloor.js
+++ imports/client/ui/student/vektors/SchoolFloor.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Meteor } from "meteor/meteor";
-import ReactSnap from "react-snap-svg";
 export default class schoolFloor extends React.Component {
   constructor(props) {
     super(props);
