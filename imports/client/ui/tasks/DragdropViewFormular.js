9ac3a14ed5b653a458c739d608a388d7551cb473
Julix89able <jdierker@uos.de>
2019-10-28 17:30:35 +0100

    new type

--- imports/client/ui/tasks/DragdropViewFormular.js
+++ imports/client/ui/tasks/DragdropViewFormular.js
@@ -1,184 +1,72 @@
 import React, { Component } from "react";
 import { Container, Draggable } from "react-smooth-dnd";
 import { applyDrag, generateItems } from "./utils";
+import { Grid, Image, Form } from "semantic-ui-react";
 
-const form = [
-  {
-    id: 0,
-    element: <h2>Form Header</h2>
-  },
-  {
-    id: 1,
-    label: "Full Name",
-    element: (
-      <div className="field-group">
-        <input type="text" />
-        <input type="text" />
-      </div>
-    )
-  },
-  {
-    id: 2,
-    label: "Email",
-    element: <input type="email" />
-  },
-  {
-    id: 3,
-    label: "Address",
-    element: <textarea name="address" id="" cols="30" rows="10" />
-  },
-  {
-    id: 5,
-    label: "Radio",
-    element: (
-      <div>
-        <div>
-          <label>
-            <input type="radio" name="r" /> option 1
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="r" /> option 2
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="r" /> option 3
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="r" /> option 4
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="radio" name="r" /> option 5
-          </label>
-        </div>
-      </div>
-    )
-  },
-  {
-    id: 4,
-    label: "Options",
-    element: (
-      <select>
-        <option value="1">Option 1</option>
-        <option value="2" selected>
-          Option 2
-        </option>
-        <option value="3">Option 3</option>
-        <option value="4">Option 4</option>
-      </select>
-    )
-  },
-  {
-    id: 6,
-    label: "Checkbox",
-    element: (
-      <div>
-        <div>
-          <label>
-            <input type="checkbox" name="r" /> option 1
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="r" /> option 2
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="r" /> option 3
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="r" /> option 4
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="r" /> option 5
-          </label>
-        </div>
-      </div>
-    )
-  },
-  {
-    id: 7,
-    element: (
-      <div>
-        <button className="form-submit-button">Submit</button>
-      </div>
-    )
-  }
-];
+const groupStyle = {
+  marginLeft: "50px",
+  flex: 1
+};
 
 export default class DragdropViewFormular extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      form
+      items1: generateItems(4, i => ({
+        id: "1" + i,
+        data: `Draggable 1 - ${i}`
+      })),
+      items2: generateItems(4, i => ({
+        id: "2" + i,
+        data: `Draggable 2 - ${i}`
+      }))
     };
   }
-  onDrop(dropResult) {
-    console.log(dropResult);
-    console.log(this);
-    console.log(this.state.form);
-    return this.setState({ form: applyDrag(this.state.form, dropResult) });
-  }
-
-  renderSolutionState() {
-    const that = this.props.that;
-    const solutions = this.props.scene.children;
-
-    solutions.map(solution => {
-      solution.children.map(child => {
-        let elem = document.getElementById(child.id);
-        elem.style.backgroundColor = "green";
-        if (
-          this.props.model.visQueue.find(elem => {
-            return elem.id === child.id;
-          }) !== undefined
-        ) {
-          elem.style.backgroundColor = "red";
-        }
-      });
-    });
-  }
-
-  generateForm(form) {
-    return form.map(item => {
-      return (
-        <Draggable key={item.id}>
-          <div className={`form-line`}>
-            <div className="label">
-              <span>{item.label}</span>
-            </div>
-            <div className="field">{item.element}</div>
-          </div>
-        </Draggable>
-      );
-    });
-  }
-
   render() {
-    console.log("formular");
-    const that = this.props.that;
     return (
-      <div className="form-demo">
-        <div className="form">
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "stretch",
+          marginTop: "50px",
+          marginRight: "50px"
+        }}
+      >
+        <div style={groupStyle}>
           <Container
-            style={{ paddingBottom: "200px" }}
-            dragClass="form-ghost"
-            dropClass="form-ghost-drop"
-            onDrop={this.onDrop.bind(this)}
-            nonDragAreaSelector=".field"
+            groupName="1"
+            getChildPayload={i => this.state.items1[i]}
+            onDrop={e =>
+              this.setState({ items1: applyDrag(this.state.items1, e) })
+            }
           >
-            {this.generateForm(this.state.form)}
+            {this.state.items1.map(p => {
+              return (
+                <Draggable key={p.id}>
+                  <div className="draggable-item">
+                    <Image src="/avatars/Lehrer.png" />
+                  </div>
+                </Draggable>
+              );
+            })}
+          </Container>
+        </div>
+        <div style={groupStyle}>
+          <Container
+            groupName="1"
+            getChildPayload={i => this.state.items2[i]}
+            onDrop={e =>
+              this.setState({ items2: applyDrag(this.state.items2, e) })
+            }
+          >
+            {this.state.items2.map(p => {
+              return (
+                <Draggable key={p.id}>
+                  <div className="draggable-item">{p.data}</div>
+                </Draggable>
+              );
+            })}
           </Container>
         </div>
       </div>
