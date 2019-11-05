afb0e6709bc6b5605af406c797db94ac0e4db737
Julix89able <jdierker@uos.de>
2019-11-05 14:47:07 +0100

    prgrs new dragtype

--- imports/client/ui/tasks/DragdropViewFormular.js
+++ imports/client/ui/tasks/DragdropViewFormular.js
@@ -1,74 +1,188 @@
-import React, { Component } from "react";
-import { Container, Draggable } from "react-smooth-dnd";
-import { applyDrag, generateItems } from "./utils";
-import { Grid, Image, Form } from "semantic-ui-react";
+import React from "react";
+import { TweenMax } from "gsap";
 
-const groupStyle = {
-  marginLeft: "50px",
-  flex: 1
-};
-
-export default class DragdropViewFormular extends Component {
+export default class DragdropViewFormular extends React.Component {
   constructor(props) {
     super(props);
+    this.view = null;
 
     this.state = {
-      items1: generateItems(4, i => ({
-        id: "1" + i,
-        data: `Draggable 1 - ${i}`
-      })),
-      items2: generateItems(4, i => ({
-        id: "2" + i,
-        data: `Draggable 2 - ${i}`
-      }))
+      statements: [
+        { id: "motivation", name: "Motivation" },
+        { id: "motiv", name: "Motiv" }
+      ],
+      images: [
+        { id: "motivationImage", name: "Motivation" },
+        { id: "motivImage", name: "Motiv" }
+      ],
+      examples: [
+        { id: "motivationExample", name: "Motivation" },
+        { id: "motivExample", name: "Motiv" }
+      ]
     };
+    this.handleLoad = this.handleLoad.bind(this);
+  }
+  handleLoad() {
+    this.initDragDrop();
+  }
+
+  updateDirections() {
+    var directionStart = document.getElementById("directionStart"),
+      directionVelocity = document.getElementById("directionVelocity"),
+      directionObject = document.getElementById("directionObject"),
+      original = document.getElementById("original"),
+      logoElement = document.getElementById("logoElement");
+    //getDirection() can return 3 types of direction...
+    directionStart.innerHTML = '"' + getDirection("start") + '"'; //direction from start of drag
+    directionVelocity.innerHTML = '"' + getDirection("velocity") + '"'; //momentary velocity *requires ThrowPropsPlugin
+    directionObject.innerHTML = '"' + getDirection(logoElement) + '"'; //direction from an object
+  }
+
+  initDragDrop() {
+    Draggable.create(".dragItem", {
+      type: "x,y",
+      onRelease: this.dropItem
+    });
+  }
+  componentDidMount() {
+    function shuffle(a) {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
+    let shuffled = shuffle(this.state.statements);
+    this.setState({ statements: shuffled });
+    window.addEventListener("load", this.handleLoad());
+  }
+
+  componentDidUpdate() {
+    this.initDragDrop();
+    if (this.props.showSolution) {
+      const nodes = [
+        document.getElementById("selfActualization_target").childNodes,
+        document.getElementById("esteem_target").childNodes,
+        document.getElementById("socialneeds_target").childNodes,
+        document.getElementById("safety_target").childNodes,
+        document.getElementById("physological_target").childNodes
+      ];
+
+      const correctArr = this.props.model.correctArr;
+
+      if (correctArr.length >= 5) {
+        for (let i = 0; i < correctArr.length; i++) {
+          for (let j = 0; j < correctArr[i].length; j++) {
+            if (nodes[i][j]) {
+              nodes[i][j].classList.add(
+                correctArr[i][j] === true ? "correct" : "false"
+              );
+            }
+          }
+        }
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("load", this.handleLoad());
+  }
+  dropItem() {
+    var boundsBefore, boundsAfter;
+    const targets = ["motivation", "motiv"];
+    let targetHit = "";
+    for (let i = 0; i < targets.length; i++) {
+      if (this.hitTest("#" + targets[i] + "_target")) {
+        targetHit = "#" + targets[i] + "_target";
+      }
+    }
+    if (targetHit !== "") {
+      boundsBefore = this.target.getBoundingClientRect();
+      $(this.target).appendTo(targetHit);
+      boundsAfter = this.target.getBoundingClientRect();
+      TweenMax.fromTo(
+        this.target,
+        0.3,
+        {
+          x: "+=" + (boundsBefore.left - boundsAfter.left),
+          y: "+=" + (boundsBefore.top - boundsAfter.top)
+        },
+        {
+          x: 0,
+          y: 0
+        }
+      );
+    } else {
+      TweenMax.to(this.target, 0.5, { x: 0, y: 0 });
+    }
+  }
+
+  renderDragContent() {
+    return (
+      <div className="defCardWrapper">
+        <div className="defCards">
+          <div id="defcard_motivation" className="selected DefCard" />
+          <div id="defcard_motiv" className="selected DefCard" />
+
+          <div className="dragItemGroup">
+            {this.state.statements.map(statement => {
+              return (
+                <div
+                  key={statement.id + "_key"}
+                  id={statement.id}
+                  className="dragItem"
+                >
+                  {statement.name}
+                </div>
+              );
+            })}
+            {this.state.images.map((image, index) => {
+              return (
+                <img
+                  src={
+                    "/tasks/Drag/Motivationsbegriffe/" +
+                    this.state.statements[index].id +
+                    ".png"
+                  }
+                  key={image.id + "_key"}
+                  id={image.id}
+                  className="dragItem"
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
+  renderTargets() {
+    return (
+      <div className="targetItemGroup">
+        {this.state.statements.map(statement => {
+          return (
+            <div class="customCard">
+              <img
+                class="customImage"
+                src="/tasks/Drag/ImagePlaceholder.png"
+                alt="Avatar"
+              />
+              <div class="customContainer">
+                <h4>
+                  <b>{statement.name}</b>
+                </h4>
+                <p>Architect & Engineer</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
   render() {
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "stretch",
-          marginTop: "50px",
-          marginRight: "50px"
-        }}
-      >
-        <div style={groupStyle}>
-          <Container
-            groupName="1"
-            getChildPayload={i => this.state.items1[i]}
-            onDrop={e =>
-              this.setState({ items1: applyDrag(this.state.items1, e) })
-            }
-          >
-            {this.state.items1.map(p => {
-              return (
-                <Draggable key={p.id}>
-                  <div className="draggable-item">
-                    <Image src="/avatars/Lehrer.png" />
-                  </div>
-                </Draggable>
-              );
-            })}
-          </Container>
-        </div>
-        <div style={groupStyle}>
-          <Container
-            groupName="1"
-            getChildPayload={i => this.state.items2[i]}
-            onDrop={e =>
-              this.setState({ items2: applyDrag(this.state.items2, e) })
-            }
-          >
-            {this.state.items2.map(p => {
-              return (
-                <Draggable key={p.id}>
-                  <div className="draggable-item">{p.data}</div>
-                </Draggable>
-              );
-            })}
-          </Container>
-        </div>
+      <div id="dragWrapper">
+        {this.renderTargets()}
+        {this.renderDragContent()}
       </div>
     );
   }
