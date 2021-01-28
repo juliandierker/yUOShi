8b1cda3fe9fb62370c27842cafdf1cab27a7ea47
SmokyZone <semau@uos.de>
2021-01-28 10:48:29 +0100

    simplify icon logic in progressBar

--- imports/ui/progressBar/progressBarSubItem.js
+++ imports/ui/progressBar/progressBarSubItem.js
@@ -6,16 +6,10 @@ import Icon from "../IconComponent/Icon"
 import "./progressBarSubItem.css"
 
 export default function ProgressBarSubItem({ title, type }) {
-  // TODO: Add correct icons!!
   let iconName = "info"
-  iconName = type === "multi" ? "checkmark-alt" : iconName
-  iconName = type === "survey" ? "checkmark-alt" : iconName
-  iconName = type === "drag" ? "info" : iconName
-  iconName = type === "card" ? "info" : iconName
-  iconName = type === "cloze" ? "info" : iconName
-  iconName = type === "memory" ? "info" : iconName
-  iconName = type === "training" ? "info" : iconName
-
+  if (type !== "text" && type !== "tag") {
+    iconName = "checkmark-alt"
+  }
 
   const icon = <Icon name={iconName} size="large"></Icon>
   return (
