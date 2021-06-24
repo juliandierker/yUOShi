e6a5c60ee6fe5573b6dd58b1ff0ea74bc2f3e713
Julian Dierker <jdierker@uni-osnabrueck.de>
2021-06-24 09:32:06 +0200

    repair cloze start (#350)

--- imports/ui/taskRenderers/RenderCloze.js
+++ imports/ui/taskRenderers/RenderCloze.js
@@ -4,11 +4,13 @@ import PromisifiedMeteor from "../../api/promisified";
 
 import Swal from "sweetalert2";
 import { useTasksContext } from "../student/TasksContext";
-import "./RenderCloze.css"
+import "./RenderCloze.css";
 
 // Fisher--Yates shuffle Algorithm
 const shuffle = (array) => {
-  let counter = array.length, temp, index;
+  let counter = array.length,
+    temp,
+    index;
   // While there are elements in the array
   while (counter > 0) {
     // Pick a random index
@@ -21,7 +23,7 @@ const shuffle = (array) => {
     array[index] = temp;
   }
   return array;
-}
+};
 
 /**
  * Render a Cloze-Task
@@ -31,17 +33,13 @@ const shuffle = (array) => {
  */
 export default function RenderCloze(props) {
   const { task } = props;
-  const { getSolution, setSolution } = useTasksContext();
-
+  const { setSolution } = useTasksContext();
+  const [userSolution, setUserSolution] = useState({});
   const [done, setDone] = useState(false);
 
-  useEffect(() => {
-    setSolution(() => onSubmit);
-  }, []);
-
-  /** @type React.FormEventHandler<HTMLFormElement> */
-  const onSubmit = useCallback(async () => {
-    const formData = new FormData(document.getElementById("clozeForm"));
+  function handleFormChange(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
     const formEntries = formData.entries();
 
     const solutions = {};
@@ -51,7 +49,24 @@ export default function RenderCloze(props) {
 
       solutions[id][inputId] = value;
     }
+    setUserSolution(solutions);
+  }
+  useEffect(() => {
+    setSolution(() => onSubmit);
+  }, [userSolution]);
 
+  /** @type React.FormEventHandler<HTMLFormElement> */
+  const onSubmit = useCallback(async () => {
+    const formData = new FormData(document.getElementById("cloze-form"));
+    const formEntries = formData.entries();
+
+    const solutions = {};
+    for (const [key, value] of formEntries) {
+      const [id, inputId] = key.split("-");
+      solutions[id] = solutions[id] || {};
+
+      solutions[id][inputId] = value;
+    }
     const result = await PromisifiedMeteor.call(
       "tasks.checkCloze",
       task.id,
@@ -79,36 +94,46 @@ export default function RenderCloze(props) {
   }, [task]);
 
   const RenderDropdownInput = ({ name, answers }) => {
+    let options = shuffle(
+      answers.map((data, index) => {
+        return (
+          <option key={"answer-" + name + "-" + index} value={data}>
+            {data}
+          </option>
+        );
+      })
+    );
 
-    let options = shuffle(answers.map((data, index) => {
-      return <option key={"answer-" + name + "-" + index} value={data}>{data}</option>
-    }))
+    return (
+      <select className="cloze-answers-dropdown" id={name} name={name}>
+        {options}
+      </select>
+    );
+  };
 
-
-    return <select className="cloze-answers-dropdown" id={name} name={name}>
-      {options}
-    </select>
-
-  }
-
-  return <div className="cloze-container">
-    <form >
-      <div className="cloze-text">
-        {task.contents.map((content) => {
-
-          return content.parts.map(({ id: answerString, content: partContent, name }, index) => {
-            let answers = []
-            if (answerString && name === "input") {
-              answers = answerString.split(";")
-            }
-            return <React.Fragment key={"cloze-content-" + index}>
-              <span>{partContent}</span>
-              {answers.length > 0 && <RenderDropdownInput name={content.id + "-" + index} answers={answers} />}
-              {name === "image" && answerString && <img alt={"Image " + index} />}
-            </React.Fragment>
-          })
-        })}
-      </div>
-    </form>
-  </div>
+  return (
+    <div className="cloze-container">
+      <form onChange={handleFormChange} id="cloze-form">
+        <div className="cloze-text">
+          {task.contents.map((content) => {
+            return content.parts.map(({ id: answerString, content: partContent, name }, index) => {
+              let answers = [];
+              if (answerString && name === "input") {
+                answers = answerString.split(";");
+              }
+              return (
+                <React.Fragment key={"cloze-content-" + index}>
+                  <span>{partContent}</span>
+                  {answers.length > 0 && (
+                    <RenderDropdownInput name={content.id + "-" + index} answers={answers} />
+                  )}
+                  {name === "image" && answerString && <img alt={"Image " + index} />}
+                </React.Fragment>
+              );
+            });
+          })}
+        </div>
+      </form>
+    </div>
+  );
 }
