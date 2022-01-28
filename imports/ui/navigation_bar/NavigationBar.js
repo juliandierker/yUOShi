f3319c9de8237c1395470704cd50a36b63339457
SmokyZone <semau@uos.de>
2022-01-28 15:22:05 +0100

    add correct use of isSolved promise

--- imports/ui/navigation_bar/NavigationBar.js
+++ imports/ui/navigation_bar/NavigationBar.js
@@ -97,16 +97,18 @@ export default function NavigationBar() {
   }
 
   async function solveTaskHandler() {
-    if (isSolved(currentTask)) {
-      Swal.fire({
-        position: "top-end",
-        type: "info",
-        title: "Die Aufgabe wurde schon gelöst.",
-        timer: 2000
-      });
-    } else {
-      solveTask();
-    }
+    isSolved(currentTask).then((res) => {
+      if (res) {
+        Swal.fire({
+          position: "top-end",
+          type: "info",
+          title: "Die Aufgabe wurde schon gelöst.",
+          timer: 2000
+        });
+      } else {
+        solveTask();
+      }
+    });
   }
 
   console.log(isSolved(currentTask));
@@ -114,7 +116,7 @@ export default function NavigationBar() {
     <div className="workspace-navigation">
       <RenderPreviousButton />
       <button
-        disabled={currentTask?.type === "tag" || isSolved(currentTask)}
+        disabled={currentTask?.type === "tag" || !isSolved(currentTask).then((res) => res)}
         onClick={solveTaskHandler}
         className="navigation-button"
         id="navigation-button-submit">
