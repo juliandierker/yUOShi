2db2f52bfbe6c0e6a09596fa344319cc36d8fccc
Sebastian Mau <SmokyZone@users.noreply.github.com>
2022-02-07 11:19:40 +0100

    fix whitescreen bug for dnd tasks (#440)
    * fix whitescreen bug for dnd tasks
    
    * add correct use of isSolved promise
    
    * remove log

--- imports/ui/navigation_bar/NavigationBar.js
+++ imports/ui/navigation_bar/NavigationBar.js
@@ -84,6 +84,9 @@ export default function NavigationBar() {
   }, [hoverNext]);
 
   async function isSolved(currentTask) {
+    if (!currentTask)
+      return false;
+
     await PromisifiedMeteor.call("tasks.isSolved", currentTask.id, (err, res) => {
       if (res) {
         return true;
@@ -94,22 +97,25 @@ export default function NavigationBar() {
   }
 
   async function solveTaskHandler() {
-    if (isSolved(currentTask)) {
-      Swal.fire({
-        position: "top-end",
-        type: "info",
-        title: "Die Aufgabe wurde schon gelöst.",
-        timer: 2000
-      });
-    } else {
-      solveTask();
-    }
+    isSolved(currentTask).then((res) => {
+      if (res) {
+        Swal.fire({
+          position: "top-end",
+          type: "info",
+          title: "Die Aufgabe wurde schon gelöst.",
+          timer: 2000
+        });
+      } else {
+        solveTask();
+      }
+    });
   }
+
   return (
     <div className="workspace-navigation">
       <RenderPreviousButton />
       <button
-        disabled={currentTask?.type === "tag" || isSolved(currentTask)}
+        disabled={currentTask?.type === "tag" || !isSolved(currentTask).then((res) => res)}
         onClick={solveTaskHandler}
         className="navigation-button"
         id="navigation-button-submit">
